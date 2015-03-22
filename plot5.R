c64c5b82a0b5032c8784bcd60c4a7610c888d96f
zajacmp3 <deejay.zajac@gmail.com>
2015-03-22 10:06:43 +0100

    Updated plots

--- plot5.R
+++ plot5.R
@@ -2,13 +2,14 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-#Merge data & filter autos data
+#Merge data
 merged <- merge(NEI, SCC[,c(1,4)], by.x = "SCC", by.y = "SCC", all.x = TRUE)
 merged$EI.Sector <- as.character(merged$EI.Sector)
-merged <- merged[grep("Vehicle", merged$EI.Sector),]
+#Filter
+filtered <- merged[grep("Vehicle", merged$EI.Sector),]
 
 #Calculate sum of emissions per year in Baltimore
-agg <- aggregate(Emissions ~ year, data = merged[merged$fips == "24510",], FUN = sum)
+agg <- aggregate(Emissions ~ year, data = filtered[filtered$fips == "24510",], FUN = sum)
 
 #make plot
 library(ggplot2)
--- plot6.R
+++ plot6.R
@@ -2,13 +2,14 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-#Merge data & filter autos data
+#Merge data
 merged <- merge(NEI, SCC[,c(1,4)], by.x = "SCC", by.y = "SCC", all.x = TRUE)
 merged$EI.Sector <- as.character(merged$EI.Sector)
-merged <- merged[grep("Vehicle", merged$EI.Sector),]
+#Filter
+filtered <- merged[grep("Vehicle", merged$EI.Sector),]
 
 #Calculate sum of emissions per year in Baltimore & Los Angeles County
-agg <- aggregate(Emissions ~ year * fips, data = merged[merged$fips == "24510" | merged$fips == "06037",], FUN = sum)
+agg <- aggregate(Emissions ~ year * fips, data = filtered[filtered$fips == "24510" | filtered$fips == "06037",], FUN = sum)
 agg$fips[agg$fips == "24510"] <- "Baltimore"
 agg$fips[agg$fips == "06037"] <- "Los Angeles"
 
